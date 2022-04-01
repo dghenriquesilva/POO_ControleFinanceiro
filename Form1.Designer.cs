0858a23d84eeaf9bf824abb4c26a9de30ca39382
Diogo henrique <dghenriquesilva@gmail.com>
2022-04-01 11:01:18 -0300

    Objeto cliente criado, com métodos de busca e gravação.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace WindowsFormsApp1
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lstvDados = new System.Windows.Forms.ListView();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,7 +43,7 @@ namespace WindowsFormsApp1
             this.arquivoToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1374, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1320, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -60,15 +61,30 @@ namespace WindowsFormsApp1
             this.sairToolStripMenuItem.Size = new System.Drawing.Size(117, 26);
             this.sairToolStripMenuItem.Text = "Sair";
             // 
+            // lstvDados
+            // 
+            this.lstvDados.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lstvDados.FullRowSelect = true;
+            this.lstvDados.GridLines = true;
+            this.lstvDados.HideSelection = false;
+            this.lstvDados.Location = new System.Drawing.Point(392, -17);
+            this.lstvDados.Name = "lstvDados";
+            this.lstvDados.Size = new System.Drawing.Size(1305, 400);
+            this.lstvDados.TabIndex = 1;
+            this.lstvDados.UseCompatibleStateImageBehavior = false;
+            this.lstvDados.View = System.Windows.Forms.View.Details;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1374, 487);
+            this.ClientSize = new System.Drawing.Size(1320, 487);
+            this.Controls.Add(this.lstvDados);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -81,6 +97,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ListView lstvDados;
     }
 }
 
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ColunasListView();
+            CelulasListView();
+        }
+        public void ColunasListView()
+        {
+
+            lstvDados.Columns.Add("ID", 5 * (int)lstvDados.Font.SizeInPoints, HorizontalAlignment.Center);
+            lstvDados.Columns.Add("CLIENTE)", 30 * (int)lstvDados.Font.SizeInPoints, HorizontalAlignment.Center);
+            lstvDados.Columns.Add("IP(S) PUBLICO(S)", 30 * (int)lstvDados.Font.SizeInPoints, HorizontalAlignment.Center);
+            lstvDados.Columns.Add("SERVIDOR RDP", 30 * (int)lstvDados.Font.SizeInPoints, HorizontalAlignment.Center);
+            lstvDados.Columns.Add("PORTA LOCAL", 30 * (int)lstvDados.Font.SizeInPoints, HorizontalAlignment.Center);
+            
+        }
+        public void CelulasListView()
+        {
+            Clientes c1 = new Clientes();
+            List<Clientes> lista = new List<Clientes>(c1.ListarClientes());
+            lstvDados.SelectedItems.Clear();
+            ListViewItem items = new ListViewItem();
+            foreach (var x in lista)
+            {
+                items.Text = x.ID.ToString();
+                items.SubItems.Add(x.Nome.ToString());
+            }
+            lstvDados.Items.Add(items);
+
+        }
+    }
+}
